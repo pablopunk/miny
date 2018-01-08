60583c1e584521c71e558f2731ec68abd04044e3
pablopunk <pablovarela182@gmail.com>
2018-01-08 12:00:55 +0100

    Name is a must

--- cli.js
+++ cli.js
@@ -5,15 +5,23 @@ const mri = require('mri')
 const sao = require('sao')
 const chalk = require('chalk')
 
-const args = mri(process.argv.slice(2))
-
 const log = msg => console.log(chalk.blue(msg))
+const err = msg => console.log(chalk.red(msg))
 
-const name = args._[0] || '.'
-log(`Creating package in ${name}`)
+function cli(args) {
+  if (!args._[0]) {
+    err('You must provide a name')
+    return
+  }
 
-sao({
-  template: __dirname,
-  targetPath: path.resolve(name),
-  mockPrompts: Boolean(args.y)
-})
+  const name = args._[0]
+  log(`Creating package in ${name}`)
+
+  sao({
+    template: __dirname,
+    targetPath: path.resolve(name),
+    mockPrompts: Boolean(args.y)
+  })
+}
+
+cli(mri(process.argv.slice(2)))
